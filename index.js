ef5ee23cceab60a3c0067e361f32fb50b10d5df7
Valtteri Kurunsaari <valtteri.kurunsaari@gmail.com>
2022-08-14 19:32:27 +0300

    Reworked Grid Script

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const colorValue = document.getElementById('color-value')
 
 let sqrAmount = 16;
 let grid = 4;
-
 let currentColor = '#000000'
 
 const squareWrap = document.createElement('div')
@@ -18,45 +17,11 @@ gridSelection.addEventListener("change", changeGrid);
 colorValue.addEventListener("change", changeColor);
 
 function changeGrid() {
-
-    switch(gridSelection.value) {
-        case "4x4":
-            sqrAmount = 16;
-            grid = 4;
-        break;
-        case "5x5":
-            sqrAmount = 25;
-            grid = 5;
-        break;
-        case "6x6":
-            sqrAmount = 36;
-            grid = 6;
-        break;
-        case "7x7":
-            sqrAmount = 49;
-            grid = 7;
-        break;
-        case "8x8":
-            sqrAmount = 64;
-            grid = 8;
-        break;
-        case "9x9":
-            sqrAmount = 81;
-            grid = 8;
-        break;
-        case "10x10":
-            sqrAmount = 100;
-            grid = 10;
-        break;
-        default:
-            sqrAmount = 16;
-            grid = 4;
-        break;
-    }
+    sqrAmount = gridSelection.value * gridSelection.value
+    grid = gridSelection.value
 
     createGrid()
     getGrid()
-
 }
 
 function changeColor(){
