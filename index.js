8d8bcea0655ed63c76b9c633d75a01058b2c16fe
Valtteri Kurunsaari <valtteri.kurunsaari@gmail.com>
2022-08-17 04:15:52 +0300

    Added ability to color while mouse is down

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const container = document.getElementById("container")
+const canvasLabel = document.getElementById('canvas-label')
 const gridSelection = document.getElementById("grid-value")
 const colorValue = document.getElementById('color-value')
+const border = document.getElementById('border')
+const eraseBtn = document.getElementById('erase')
 
 let sqrAmount = 16;
 let grid = 4;
+canvasLabel.textContent = `Canvas Size ${gridSelection.value}x${gridSelection.value}`
 let currentColor = '#000000'
+border.checked = true;
+let isMouseDown = false
 
 const squareWrap = document.createElement('div')
 squareWrap.classList.add('square-wrapper')
 container.appendChild(squareWrap)
 
 window.addEventListener('load', createGrid)
-window.addEventListener('load', getGrid)
+window.addEventListener('load', colorGrid)
 
-gridSelection.addEventListener("change", changeGrid);
-colorValue.addEventListener("change", changeColor);
+gridSelection.onchange = () => (changeGrid())
+colorValue.onchange = () => (changeColor())
+eraseBtn.onclick = () => (changeGrid())
+
+document.body.onmousedown = () => (isMouseDown = true)
+document.body.onmouseup = () => (isMouseDown = false)
 
 function changeGrid() {
+    canvasLabel.textContent = `Canvas Size ${gridSelection.value}x${gridSelection.value}`
     sqrAmount = gridSelection.value * gridSelection.value
     grid = gridSelection.value
-
     createGrid()
-    getGrid()
+    colorGrid()
 }
 
 function changeColor(){
@@ -30,31 +40,58 @@ function changeColor(){
 
 function createGrid() {
     let output = "";
-
     for(let i = 0; i < sqrAmount; i++) {
         output += `
-            <div id="square-${i}" class="square"></div>
+            <div id="square-${i}" class="square border-active"></div>
         `;
     }
-    squareWrap.innerHTML = output;
-            
+    squareWrap.innerHTML = output;          
     squareWrap.style.gridTemplate = `repeat(${grid}, 1fr) / repeat(${grid}, 1fr)`
 }
 
-function getGrid() {
+function colorGrid() {
     const squares = document.querySelectorAll('.square')
 
+    
     squares.forEach((square) => {
-        square.addEventListener('click', (e) => {
+        ['mousedown','mouseover'].forEach( evt => {
+            square.addEventListener(evt, hoverColor, false)
+        });
+    })
 
-            if (e.target.classList[1] == 'colored') {
+    /*squares.forEach((square) => {
+        square.addEventListener('mousedown', (e) => {
+
+            if (e.target.classList[2] == 'colored') {
                 e.target.classList.remove('colored')
                 e.target.style.backgroundColor = 'white'
             }
             else {
                 e.target.classList.add('colored')
                 e.target.style.backgroundColor = currentColor
+                console.log(e.type)
+                console.log(isMouseDown)
             }
         });
-    });
+    });*/
+
+}
+
+function hoverColor(e) {
+    if (e.target.classList[2] == 'colored') {
+        if (e.type === 'mousedown') {
+            e.target.classList.remove('colored')
+            e.target.style.backgroundColor = 'white'
+        }
+    }
+    else {
+        if(isMouseDown == true && e.type === 'mouseover')  {
+            e.target.classList.add('colored')
+            e.target.style.backgroundColor = currentColor
+        }
+        else if (e.type === 'mousedown') {
+            e.target.classList.add('colored')
+            e.target.style.backgroundColor = currentColor
+        }
+    }
 }
